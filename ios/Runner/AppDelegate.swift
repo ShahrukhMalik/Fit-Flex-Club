b012645d7f683e6718c6f1f0e23073d0fd03d4ea
Omkar <comkar.prj@tmf.co.in>
2025-04-23 19:04:14 +0530

    feat : push notification config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,17 +1,26 @@
 import Flutter
 import UIKit
 import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken:Data){
+      Messaging.messaging().apnsToken = deviceToken
+      super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+  
 }
