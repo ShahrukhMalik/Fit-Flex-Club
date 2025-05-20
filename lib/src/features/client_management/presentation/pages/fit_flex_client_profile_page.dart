a1d1dc6b35afa2a21dae43b3ad4e6d5bd232220f
Omkar <comkar.prj@tmf.co.in>
2025-05-20 10:46:07 +0530

    fix : bottom padding for weight tracker

--- lib/src/features/client_management/presentation/pages/fit_flex_client_profile_page.dart
+++ lib/src/features/client_management/presentation/pages/fit_flex_client_profile_page.dart
@@ -1177,6 +1177,9 @@ class _FitFlexClientProfilePageState extends State<FitFlexClientProfilePage> {
                   );
                 }
               },
+            ),
+            SizedBox(
+              height: 120,
             )
           ],
         ),
